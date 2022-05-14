0206619253aa1f13e373ff9689f8bb8b54c42265
(YC) YUN CHANG LEE <lancelee92@gmail.com>
2022-05-14 21:43:25 +0800

    Feat: added URL Shortener

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const dns = require('dns');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+mongoose.connect('mongodb+srv://lancelee92:29EWJkVvv6LsUa9t@cluster0.kzzhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+const shortenerSchema = new Schema({
+  url:  String, // String is shorthand for {type: String}  
+});
+
+const shortenedUrl = mongoose.model('shortenedUrl', shortenerSchema);
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +30,60 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  //console.log(req.params.id);
+  shortenedUrl.findOne({ _id: req.params.id }, (err, doc) => {
+    if(err)
+      res.json({ error: err });
+
+    //console.log(doc.url);
+    res.redirect(doc.url);    
+  });
+});
+
+app.get('/api/shorturl', function(req, res) {
+
+  shortenedUrl.find().exec((err, doc) => {
+    if(err)
+      res.json({ greeting: 'hello API' });
+    else
+      res.json(doc);
+  })
+  
+});
+
+app.post('/api/shorturl', (req, res) => {
+  console.log(req.body);
+  if(req.body.url){
+
+    const options = {
+        all:true,
+    };
+  
+    dns.lookup(req.body.url, options, (err, addresses) => {
+      if(err){
+        if(err.code == 'ENOTFOUND')
+          return res.json({ error: 'invalid url' });
+      }
+      else {
+        const newUrl = new shortenedUrl();
+        newUrl.url = req.body.url;
+    
+        newUrl.save((err, doc) => {
+          if(err)
+            console.error(err);
+    
+          res.json({"original_url":req.body.url,"short_url":doc['_id']})
+        });    
+      }
+      
+    });
+        
+    
+    
+  }
 });
 
 app.listen(port, function() {
